820381c2498976c1603b3d97b1e97ea33100f97f
Rui Vilela <tek@qwertytek.com>
2024-10-29 02:19:37 +0000

    finished logic in main.py

--- database.py
+++ database.py
@@ -6,6 +6,6 @@ SQLALCHEMY_DATABASE_URL = 'sqlite:///./auth.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
-sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, status, HTTPException
+from database import engine, SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
+import models
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def user(user: None, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication failed")
+    return {
+        "User": user
+    }
